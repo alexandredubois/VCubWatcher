90508d36ab3d9b8ab61b33dde3a0c4f4295228f0
Alexandre Dubois <contact@alexandredubois.com>
2021-03-18 11:57:05 +0100

    Branchement API et affichage de la liste des stations

--- Controllers/StationController.cs
+++ Controllers/StationController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using VCubWatcher.Models;
 
 namespace VCubWatcher.Controllers
 {
@@ -10,12 +13,30 @@ namespace VCubWatcher.Controllers
     {
         public IActionResult Liste()
         {
-            return View();
+            var stations = GetBikeStationsFromApi();
+            return View(stations);
         }
 
         public IActionResult Carte()
         {
             return View();
+        }
+
+        private static List<BikeStation> GetBikeStationsFromApi()
+        {
+            //Création un HttpClient (= outil qui va permettre d'interroger une URl via une requête HTTP)
+            using (var client = new HttpClient())
+            {
+                //Interrogation de l'URL censée me retourner les données
+                var response = client.GetAsync("http://api.alexandredubois.com/vcub-backend/vcub.php");
+                //Récupération du corps de la réponse HTTP sous forme de chaîne de caractères
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                //Conversion de mon flux JSON (string) en une collection d'objets BikeStation
+                //d'un flux de données vers des objets => Déserialisation
+                //d'objets vers un flux de données => Sérialisation
+                var result = JsonConvert.DeserializeObject<List<BikeStation>>(stringResult.Result);
+                return result;
+            }
         }
     }
 }
---
+++ Models/BikeStation.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace VCubWatcher.Models
+{
+    public class BikeStation
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("bike_count")]
+        public int BikeCount { get; set; }
+
+        [JsonProperty("electric_bike_count")]
+        public int ElectricBikeCount { get; set; }
+
+        [JsonProperty("bike_count_total")]
+        public int BikeCountTotal { get; set; }
+
+        [JsonProperty("slot_count")]
+        public int SlotCount { get; set; }
+
+        [JsonProperty("is_online")]
+        public bool IsOnline { get; set; }
+
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+    }
+}
