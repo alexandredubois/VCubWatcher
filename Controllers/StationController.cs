1b76842b2f97b5377bcbc3f52481e166e94d440e
Alexandre Dubois <contact@alexandredubois.com>
2021-03-23 16:31:37 +0100

    Manage favorite stations

--- Controllers/StationController.cs
+++ Controllers/StationController.cs
@@ -1,25 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using VCubWatcher.Models;
 
 namespace VCubWatcher.Controllers
 {
     public class StationController : Controller
     {
+        private const string _cookieKey = "stations";
+
         public IActionResult Liste()
         {
             var stations = GetBikeStationsFromApi();
+
+
+
             return View(stations);
         }
 
         public IActionResult Carte()
         {
-            return View();
+            var stations = GetBikeStationsFromApi();
+            return View(stations);
+        }
+
+        public IActionResult Favoris()
+        {
+            //On récupère les 183 stations
+            var stations = GetBikeStationsFromApi();
+            
+            //On récupère la liste des stations en favoris
+            List<int> favoriteStationIds ;
+            if (Request.Cookies.ContainsKey(_cookieKey))
+            {
+                favoriteStationIds = JsonConvert.DeserializeObject<List<int>>(Request.Cookies[_cookieKey]);
+            } else
+            {
+                favoriteStationIds = new List<int>();
+            }
+
+            //On crée une liste de station vide dans laquelle on copiera les stations dont les IDs sont dans le cookie
+            List<BikeStation> favorites = new List<BikeStation>();
+            foreach (var stationId in favoriteStationIds)
+            {
+                foreach (var station in stations)
+                {
+                    if (station.Id == stationId) favorites.Add(station);
+                }
+            }
+
+            return View(favorites);
         }
 
         private static List<BikeStation> GetBikeStationsFromApi()
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,16 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace VCubWatcher.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Favoris()
+        private const string _cookieKey = "stations";
+
+        public IActionResult AddFavorite(int stationId)
         {
-            return View();
+            List<int> favorites;
+
+            //1) Récupérer la valeur actuelle du cookie qui nous intéresse
+            if (Request.Cookies.ContainsKey(_cookieKey))
+            {
+                favorites = JsonConvert.DeserializeObject<List<int>>(Request.Cookies[_cookieKey]);
+            }
+            else
+            {
+                favorites = new List<int>();
+            }
+
+            //Si la station n'existe pas dans la liste, je l'ajoute
+            if (!favorites.Contains(stationId))
+            {
+                favorites.Add(stationId);
+            }
+
+            //Je supprime le cookie avec sa valeur actuelle
+            Response.Cookies.Delete(_cookieKey);
+
+            //Je crée un cookie de même nom avec la nouvelle valeur
+            Response.Cookies.Append(_cookieKey, JsonConvert.SerializeObject(favorites));
+
+            //On redirige l'utilisateur vers une action d'un autre contrôleur
+            return RedirectToAction("Favoris", "Station");
+        }
+
+        public IActionResult RemoveFavorite(int stationId)
+        {
+            List<int> favorites;
+            //1) Récupérer la valeur actuelle du cookie qui nous intéresse
+            if (Request.Cookies.ContainsKey(_cookieKey))
+            {
+                favorites = JsonConvert.DeserializeObject<List<int>>(Request.Cookies[_cookieKey]);
+                if (favorites.Contains(stationId))
+                {
+                    favorites.Remove(stationId);
+                    Response.Cookies.Append(_cookieKey, JsonConvert.SerializeObject(favorites));
+                }
+                Response.Cookies.Delete(_cookieKey);
+                Response.Cookies.Append(_cookieKey, JsonConvert.SerializeObject(favorites));
+            }
+            return RedirectToAction("Favoris", "Station");
         }
     }
 }
